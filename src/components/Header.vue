b52ab75036327f6a39109a6c806353ddf2e2777e
Mark Taratynov <mark.taratynov@gmail.com>
2023-12-31 18:04:08 +0100

    header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,38 +25,51 @@
 </script>
 
 <style scoped>
-    .headbar {
-        width: 100%;
-        margin: 20px 0;
-        /* padding: 5px; */
-        display: flex;
-        justify-content: space-evenly;
-        text-align: center;
-        border-bottom: .5px solid black;
-    }
-    .headbar h3 {
-        display: inline-block;
-        border: .5px solid grey;
-        padding: 5px;
-        margin: 0 0 10px;
-        color:  teal;
-        transition: .4s;
-        cursor: pointer;
-    }
-    h3:hover {
-        color: white;
-        transition: .4s;
-    }
-    button {
-        width: 35px;
-        padding: 5px;
-        background: none;
-        font-size: 25px;
-        border: none;
-        cursor: pointer;
-    }
-    button:hover {
-        border-bottom: 2px solid black;
-        box-sizing: border-box;
-    }
+  .headbar {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 12px 20px;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  .headbar h3 {
+    margin: 0;
+    padding: 0;
+    font-size: 1.2rem;
+    color: #1da1f2;
+    cursor: pointer;
+    transition: color 0.4s;
+  }
+
+  .headbar h3:hover {
+    color: #0c8de4;
+  }
+
+  button {
+    width: 35px;
+    height: 35px;
+    padding: 0;
+    background: none;
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+    transition: border-bottom 0.4s;
+  }
+
+  button:hover {
+    border-bottom: 2px solid #1da1f2;
+    box-sizing: border-box;
+  }
+
+  .home {
+    color: #1da1f2;
+    transition: color 0.4s;
+  }
+
+  .home:hover {
+    color: #0c8de4;
+  }
 </style>
