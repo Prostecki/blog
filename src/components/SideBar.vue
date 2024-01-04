41d6f15905fbbf43755c52bbba09a5388178c201
Mark Taratynov <mark.taratynov@gmail.com>
2024-01-04 21:59:26 +0100

    sidebar content

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,40 +1,57 @@
 <template>
-    <!-- Sidebar component -->
-    <aside :style="{ transform: 'translateX(' + (sidebarOpen ? '0px' : '-300px') + ')' }" class="sidebar">
-        <!-- Close button for the sidebar -->
-        <button type="button" @click="closeSidebarHandler">X</button>
-        <!-- Author's name -->
-        <h1>{{ author }}</h1>
-        <!-- Message in the sidebar -->
-        <p>{{ message }}</p>
-    </aside>
+  <!-- Sidebar component -->
+  <aside :style="{ transform: 'translateX(' + (sidebarOpen ? '0px' : '-300px') + ')' }" class="sidebar">
+    <!-- Close button for the sidebar -->
+    <button type="button" @click="closeSidebarHandler">X</button>
+    <!-- Author's name -->
+    <h1>{{ author }}</h1>
+    <!-- Message in the sidebar -->
+    <p>{{ message }}</p>
+    
+    <!-- Additional information in the sidebar -->
+    <div>
+      <h2>Contact Information</h2>
+      <ul>
+        <li>Email: {{ contact.email }}</li>
+        <li>Twitter: <a :href="contact.twitter" target="_blank">{{ contact.twitterHandle }}</a></li>
+      </ul>
+    </div>
+
+    <!-- Any other dynamic content or data you want to display -->
+  </aside>
 </template>
 
 <script>
-    export default {
-        // Component name
-        name: "SideBar",
-        // Props for receiving data from the parent component
-        props: {
-            sidebarOpen: Boolean,   // Boolean prop indicating if the sidebar is open
-            closeSidebar: Function, // Function prop to close the sidebar
-        },
-        // Methods for handling actions in the component
-        methods: {
-            // Method to handle closing the sidebar
-            closeSidebarHandler() {
-                console.log('Closed');  // Log message when sidebar is closed
-                this.closeSidebar();     // Call the provided closeSidebar function
-            }
-        },
-        // Data properties for storing component-specific data
-        data() {
-            return {
-                author: "Prostecki",    // Author's name
-                message: "Hey everyone!", // Message in the sidebar
-            }
-        },
-    }
+export default {
+  // Component name
+  name: "SideBar",
+  // Props for receiving data from the parent component
+  props: {
+    sidebarOpen: Boolean, // Boolean prop indicating if the sidebar is open
+    closeSidebar: Function, // Function prop to close the sidebar
+  },
+  // Methods for handling actions in the component
+  methods: {
+    // Method to handle closing the sidebar
+    closeSidebarHandler() {
+      console.log('Closed'); // Log message when sidebar is closed
+      this.closeSidebar(); // Call the provided closeSidebar function
+    },
+  },
+  // Data properties for storing component-specific data
+  data() {
+    return {
+      author: "Prostecki", // Author's name
+      message: "Hey everyone!", // Message in the sidebar
+      contact: {
+        email: "prostecki@example.com",
+        twitterHandle: "@prostecki",
+        twitter: "https://twitter.com/prostecki",
+      },
+      // Any other data you want to include in the sidebar
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -76,4 +93,3 @@
     color: #1da1f2; /* Twitter's blue color */
   }
 </style>
-
